53505438dc9cfba82d205ad623c915d2ca4e4bae
jh <jh@Baeks-MacBook-Pro.local>
2022-07-06 23:20:14 +0900

    연락처 업데이트

--- pages/support.js
+++ pages/support.js
@@ -32,8 +32,9 @@ export default (props) => {
             <div>
               <img src="/svgs/phone-call.svg" alt=""></img>
               <div>
-                <h1>영업팀 권순광 과장</h1>
+                <h1>영업팀 김태민 팀장</h1>
                 <p>TEL. 031-491-9406</p>
+                <p>TEL. 010-9260-7761</p>
                 <p>Email. sunright99@hanmail.net</p>
               </div>
             </div>
